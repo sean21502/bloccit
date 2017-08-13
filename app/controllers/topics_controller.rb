712d5258f7097b82fbdcf5165df8043121e0cc1e
Sean Hite <seanhite@gmail.com>
2017-08-12 20:20:00 -0400

    checkpoint 27

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,5 +1,8 @@
 class TopicsController < ApplicationController
 	
+	before_action :require_sign_in, except: [:index, :show]
+   	before_action :authorize_user, except: [:index, :show]
+	
 	def index
      	@topics = Topic.all
    	end
@@ -50,10 +53,18 @@ class TopicsController < ApplicationController
        		render :show
      	end
    	end
-	# remember to add private methods to the bottom of the file. Any method defined below private, will be private.
+	
+# remember to add private methods to the bottom of the file. Any method defined below private, will be private.
 	private
    	
 	def topic_params
-     params.require(:topic).permit(:name, :description, :public)
-   end
+     	params.require(:topic).permit(:name, :description, :public)
+   	end
+	
+	def authorize_user
+     	unless current_user.admin?
+       		flash[:alert] = "You must be an admin to do that."
+       		redirect_to topics_path
+     	end
+   	end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
 	
 	has_many :posts, dependent: :destroy
 	before_save { self.email = email.downcase if email.present? }
+	before_save { self.role ||= :member }
 
    	validates :name, length: { minimum: 1, maximum: 100 }, presence: true
 
@@ -12,4 +13,5 @@ class User < ApplicationRecord
              uniqueness: { case_sensitive: false },
              length: { minimum: 3, maximum: 254 }
    	has_secure_password
+	enum role: [:member, :admin]
 end
