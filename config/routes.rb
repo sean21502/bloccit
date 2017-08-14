2be3978d41100d32782439c26f1660731373fd9e
Sean Hite <seanhite@gmail.com>
2017-08-14 17:20:08 -0400

    assignment 27

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   
   	resources :topics do
-    resources :posts, except: [:index]
+      resources :posts, except: [:index]
   	end
 	
 	resources :users, only: [:new, :create]
