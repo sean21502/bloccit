d3d03f1e87b2af74f4da1c76ff33fefec7bc90d9
Sean Hite <seanhite@gmail.com>
2017-08-17 22:35:10 -0400

    assignment 31a

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,3 +1,15 @@
 module UsersHelper
+	def user_has_posts?
+    	@user.posts.empty?
+	end
 	
+	def user_has_comments?
+		@user.comments.empty?
+	end
+	
+	def user_has_favorites?
+		@user.avatar_url(128)
+		@user.posts.count
+		@user.comments.count
+	end
 end
